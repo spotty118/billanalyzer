f5ae7baac01b1ebfd68880bcf3ef328c56003f68
spotty118 <spotty118@gmail.com>
2025-03-05 19:59:08 -0600

    feat: Frontend Agent generated src/components/BillDisplay.tsx

---
+++ src/components/BillDisplay.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+    const BillDisplay = () => {
+      return (
+        <div>
+          <h3>Analyzed Bill Data</h3>
+          {/* Placeholder for bill data will go here */}
+          <p>Data will be displayed here after processing.</p>
+        </div>
+      );
+    };
+
+    export default BillDisplay;
